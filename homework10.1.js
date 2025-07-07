fcf616d49a2071e9932c5b2d7bd0051038d031d2
Nataliia_Sokolova <natasokolova0897@gmail.com>
2025-07-07 12:42:38 +0300

    Homework10.1

---
+++ homework10.1.js
@@ -0,0 +1,30 @@
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+function testEmail(array){
+      let result=[]
+     for(let i=0;i<array.length;i++){
+    let email=/\b[\w.-]+@(gmail|yahoo)\.com\b/
+    let res=array[i].email.match(email)
+          if (res!=null){
+              result.push(array[i].email)
+          }
+     }
+    return result
+ }
+ 
+ console.log(testEmail(arr))
